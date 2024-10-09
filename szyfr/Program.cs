7b0716dc61538b1b700e0f67ef9cc8a235352d32
vlady <vlady@SHAPIK_JR>
2024-10-09 20:58:10 +0200

    Add project files.

---
+++ szyfr/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.InputEncoding = Encoding.UTF8;
+
+string alfabet = "aąbcćdeęfghijklłmnoópqrsśtuvwxyzźż"; // length = 34
+
+Console.WriteLine("naciśnij \"s\" żeby zaszyfrować, albo \"d\" żeby deszyfrować");
+string? option = Console.ReadLine();
+option.ToLower().Trim();
+Console.WriteLine("kod szyfru (większy od 0 mniejszy od 34)");
+string? kodSzyfru = Console.ReadLine();
+int.TryParse(kodSzyfru, out int result);
+if (result >= alfabet.Length || result < 0)
+{
+    Console.WriteLine("Kod szyfru ma być większy od 0 i mniejszy 34 (ilość liter w alfabecie)");
+    return;
+}
+Console.WriteLine("Tekst do zaszyfrowania/deszyfrowania");
+string? toChange = Console.ReadLine();
+
+
+string res = "";
+for (int i = 0; i < toChange.Length; i++)
+{
+    int resultIndex;
+    if (alfabet.Contains(toChange[i]))
+    {
+        int index = alfabet.IndexOf(toChange[i]);
+        if(option == "s")
+        {
+            resultIndex = index + result;
+            if (resultIndex >= alfabet.Length)
+            {
+                resultIndex -= alfabet.Length;
+            }
+        }
+        else if (option == "d")
+        {
+            resultIndex = index - result;
+            if (resultIndex < 0)
+            {
+                resultIndex += alfabet.Length;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Something went wrong:(");
+            return;
+        }
+        res += alfabet[resultIndex];
+    }
+}
+Console.WriteLine(res);
