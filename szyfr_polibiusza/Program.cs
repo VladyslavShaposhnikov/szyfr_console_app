9fc4e4cf776f04e44249f818fa86f58553f3aefc
Vladyslav Shaposhnikov <vladyslav.shaposhnik@microsoft.wsei.edu.pl>
2024-10-25 19:58:34 +0200

    add RandomKeyGeneration function and comments

--- szyfr_polibiusza/Program.cs
+++ szyfr_polibiusza/Program.cs
@@ -5,28 +5,10 @@ Console.InputEncoding = Encoding.UTF8;
 
 string alfabet = "aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż"; // length = 35
 
-char[,] tab = new char[5,7];
+char[,] tab = RandomKeyGeneration(alfabet);
 
-char[] charArray = alfabet.ToCharArray();
-
-Random rand = new Random();
-
-int alfabetLength = alfabet.Length;
-
-for (int i = 0; i < tab.GetLength(0); i++)
-{
-    for (int j = 0; j < tab.GetLength(1); j++)
-    {
-        char insert = charArray[rand.Next(0, alfabetLength)];
-        alfabetLength--;
-        tab[i, j] = insert;
-
-        MoveIndexInArray(charArray, insert);
-
-    }
-}
-
-string res = "";
+// test code: add each element from random generated key to the string; convert chars to int; edd all chars in both strings; check if results are equal
+string res = ""; 
 int res1 = 0;
 int res2 = 0;
 
@@ -47,8 +29,40 @@ for (int i = 0; i < alfabet.Length; i++)
 }
 
 Console.WriteLine(res1 == res2);
+Console.WriteLine(res1 + " = " + res2);
 
 
+// Random key generation function (for 2 demention array 5x7)
+char[,] RandomKeyGeneration(string alf)
+{
+    char[,] tabKey = new char[5, 7];
+
+    char[] charArr = alf.ToCharArray();
+
+    Random rand = new Random();
+
+    int alfLength = alf.Length;
+
+    for (int i = 0; i < tabKey.GetLength(0); i++)
+    {
+        for (int j = 0; j < tabKey.GetLength(1); j++)
+        {
+            char insert = charArr[rand.Next(0, alfLength)]; // random element
+
+            MoveIndexInArray(charArr, insert); // moving selected element to the end of array
+
+            alfLength--; // decrease length by 1 (to not select this element in next iteration)
+
+            tabKey[i, j] = insert;
+
+
+        }
+    }
+
+    return tabKey;
+}
+
+// function to move element which was selected randomly to the end of the array (to not take it again)
 void MoveIndexInArray(char[] charArr, char insertedChar)
 {
     int moveIndex = Array.IndexOf(charArr, insertedChar);
